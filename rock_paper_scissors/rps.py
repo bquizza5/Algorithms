1ce2de8c1ed322378965a8ae04697b312c123b40
bquizza5 <blaine555@gmail.com>
2019-12-12 10:13:40 -0700

    rps done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,12 +3,29 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
-
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
+  
+  if n == 1:
+    return [['rock'],['paper'],['scissors']]
+  elif n == 0:
+    return [[]]
   else:
-    print('Usage: rps.py [num_plays]')
+    newArr = []
+    for i in rock_paper_scissors(n-1):
+      newArr.append(i + ['rock'])
+      newArr.append(i + ['paper'])
+      newArr.append(i + ['scissors'])
+      
+    return newArr
+
+
+# print(rock_paper_scissors(0))
+
+
+
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_plays = int(sys.argv[1])
+#     print(rock_paper_scissors(num_plays))
+#   else:
+#     print('Usage: rps.py [num_plays]')
